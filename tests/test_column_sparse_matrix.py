ddde39f11c8fddf951aa8213195ab2e01961b36d
David Kleiven <davidkleiven446@gmail.com>
2019-07-30 13:26:24 +0200

    Use unittest

--- tests/test_column_sparse_matrix.py
+++ tests/test_column_sparse_matrix.py
@@ -1,28 +1,30 @@
 from clease.column_sparse_matrix import ColumnSparseMatrix
 import numpy as np
-from ase.test import must_raise
+import unittest
 
 
-def test_insert_access():
-    columns = np.array([0, 4, 7, 3])
-    values = [
-        (0, 4),
-        (4, -1),
-        (7, 4),
-        (7, 6),
-        (3, -1)
-    ]
+class TestColumnSparseMatrix(unittest.TestCase):
+    def test_insert_access(self):
+        columns = np.array([0, 4, 7, 3])
+        values = [
+            (0, 4),
+            (4, -1),
+            (7, 4),
+            (7, 6),
+            (3, -1)
+        ]
 
-    num_rows = len(values)
-    sp_mat = ColumnSparseMatrix(num_rows, columns)
+        num_rows = len(values)
+        sp_mat = ColumnSparseMatrix(num_rows, columns)
 
-    for row, val in enumerate(values):
-        sp_mat.insert(row, val[0], val[1])
-    for row, val in enumerate(values):
-        assert sp_mat.get(row, val[0]) == val[1]
+        for row, val in enumerate(values):
+            sp_mat.insert(row, val[0], val[1])
+        for row, val in enumerate(values):
+            self.assertEqual(sp_mat.get(row, val[0]), val[1])
 
-    with must_raise(ValueError):
-        sp_mat.insert(2, 2, 1)
+        with self.assertRaises(ValueError):
+            sp_mat.insert(2, 2, 1)
 
 
-test_insert_access()
+if __name__ == '__main__':
+    unittest.main()
