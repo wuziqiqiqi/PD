d8eaf53f572c6a28bdf81e94ba9da87b1bd6e2c6
David Kleiven <davidkleiven446@gmail.com>
2019-08-05 08:56:16 +0200

    Store temporary backup files in hidden folder

--- clease/gui/fittingAlgorithmEditors.py
+++ clease/gui/fittingAlgorithmEditors.py
@@ -2,15 +2,23 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.textinput import TextInput
 from kivy.uix.spinner import Spinner
+import os
 
 
 class FitAlgEditor(FloatLayout):
     close = ObjectProperty(None)
     backup_file = 'fit_alg_editor.txt'
+    backup_folder = '.cleaseGUI/'
 
     def __init__(self, **kwargs):
         FloatLayout.__init__(self, **kwargs)
+        if not os.path.exists(self.backup_folder):
+            os.mkdir(self.backup_folder)
         self.load_values()
+
+    @property
+    def backup_filename(self):
+        return self.backup_folder + self.backup_file
 
     def backup(self):
         values = []
@@ -19,13 +27,13 @@ class FitAlgEditor(FloatLayout):
                 txt = child.text
                 values.append(txt)
 
-        with open(self.backup_file, 'w') as out:
+        with open(self.backup_filename, 'w') as out:
             for v in values:
                 out.write('{}\n'.format(v))
 
     def load_values(self):
         try:
-            with open(self.backup_file, 'r') as infile:
+            with open(self.backup_filename, 'r') as infile:
                 lines = infile.readlines()
             lines = [line.strip() for line in lines]
 
