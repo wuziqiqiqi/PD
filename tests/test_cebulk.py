ac365d7c1602a6ad69e8243fed05999c19982def
David Kleiven <davidkleiven446@gmail.com>
2019-08-06 09:22:35 +0200

    Don't use an exact distance

--- tests/test_cebulk.py
+++ tests/test_cebulk.py
@@ -302,7 +302,7 @@ class TestCEBulk(unittest.TestCase):
                          concentration=concentration,
                          db_name=db_name,
                          max_cluster_size=3,
-                         max_cluster_dia=[4.0, 4.0],
+                         max_cluster_dia=[4.01, 4.01],
                          ignore_background_atoms=True)
         self.assertEqual(setting.num_basis, 2)
         self.assertEqual(len(setting.index_by_basis), 2)
