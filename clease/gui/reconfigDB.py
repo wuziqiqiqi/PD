8879056aa4614e7104895101b1193f85d3065b4a
Jin Chang <jchang@dtu.dk>
2019-09-17 22:31:26 +0200

    reconfigDB implement

--- clease/gui/reconfigDB.py
+++ clease/gui/reconfigDB.py
@@ -3,32 +3,46 @@ from kivy.app import App
 
 class ReconfigDB(object):
     """Reconfigure settings and correlation functions stored in DB."""
-    reconfig_in_progress = False
+    # reconfig_in_progress = False
     settings = None
-    sm = App.get_running_app().root.ids.sm
-    status = App.get_running_app().root.ids.status
+    app = None
+    status = None
 
     def apply_settings(self):
-        if self.reconfig_in_progress:
-            # Do no allow user to initialize many threads
-            return
-        self.reconfig_in_progress = True
 
-        self.status.text = "Reconfiguring in progress..."
-
-        if not self.sm.get_screen('Settings').apply_settings():
+        if not self.app.root.ids.sm.get_screen('Settings').apply_settings():
             msg = "Couldn't initialize settings based on the values specified "
             msg += "in the fields on Concentration\nand Settings panels. "
             msg += "Please ensure that all of the values are correct."
             self.status.text = msg
             return False
+
+        self.status.text = "Reconfiguring..."
         return True
 
     def reconfig_settings(self):
         if self.apply_settings():
-            print('hello')
-            self.settings.reconfigure_settings()
-            msg = "Cluster data updated for all templates.\nPlease also "
-            msg += "reconfigure DB entries if there are any structures stored "
-            msg += "in DB."
-            self.status.text = msg
+            try:
+                self.app.root.settings.reconfigure_settings()
+                msg = "Cluster data updated for all templates.\nPlease also "
+                msg += "reconfigure DB entries if there are any structures "
+                msg += "stored in DB."
+                self.status.text = msg
+
+            except Exception as exc:
+                self.status.text = str(exc)
+
+        self.app.root.reconfig_in_progress = False
+
+    def reconfig_db(self):
+        if self.apply_settings():
+            try:
+                from clease import CorrFunction
+                CorrFunction(self.app.root.settings).reconfigure_db_entries()
+                msg = "All DB entries are reconfigured."
+                self.status.text = msg
+
+            except Exception as exc:
+                self.status.text = str(exc)
+
+        self.app.root.reconfig_in_progress = False
