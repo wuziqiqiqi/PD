8f2e8f191886e13d617f1edcf2af4690d18349c6
David Kleiven <davidkleiven446@gmail.com>
2019-09-23 13:13:01 +0200

    Add test for factorize function

--- clease/tools.py
+++ clease/tools.py
@@ -464,6 +464,6 @@ def factorize(n):
     while n > 1:
         for i in range(2, n + 1):
             if n % i == 0:
-                n /= i
+                n = int(n/i)
                 yield i
                 break
