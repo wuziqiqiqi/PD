0fd3c031c0cf9a9d5fefe27ab7e13df72c0fdbdb
David Kleiven <davidkleiven446@gmail.com>
2019-10-02 09:14:55 +0200

    Add cases for coordinate basis

--- tests/test_gui_tools.py
+++ tests/test_gui_tools.py
@@ -2,6 +2,7 @@ import unittest
 from clease.gui.util import parse_temperature_list
 from clease.gui.util import parse_concentration_list
 from clease.gui.util import parse_cellpar, parse_cell
+from clease.gui.util import parse_coordinate_basis
 import numpy as np
 
 
@@ -50,7 +51,7 @@ class TestGUIUtil(unittest.TestCase):
         values = parse_cell('[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]')
         expected = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
         self.assertTrue(np.allclose(expected, values))
-    
+
     def test_cell_tuple_of_lists(self):
         values = parse_cell('([1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0])')
         expected = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
@@ -70,6 +71,37 @@ class TestGUIUtil(unittest.TestCase):
         values = parse_cell('[[[ [3.0, 2.0, 1.0], (4.0, 5, 6.0), (7.0, 8, 9.0) ]]] ')
         expected = [[3.0, 2.0, 1.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
         self.assertTrue(np.allclose(expected, values))
+
+    def test_crd_basis_two_tuples(self):
+        values = parse_coordinate_basis('(1.0, 4.0, 4.0), (2.0, 1.0, 6.0)')
+        expected = [[1.0, 4.0, 4.0], [2.0, 1.0, 6.0]]
+        self.assertTrue(np.allclose(values, expected))
+
+    def test_crd_basis_two_many_coordinates(self):
+        with self.assertRaises(ValueError):
+            parse_coordinate_basis('(1.0, 2.0, 3.0), (4.0, 5.0, 6.0), (7.0, 8.0, 9.0, 1.0)')
+
+    def test_crd_basis_three_lists(self):
+        values = parse_coordinate_basis('[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]')
+        expected = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
+        self.assertTrue(np.allclose(values, expected))
+
+    def test_crd_basis_tuple_of_lists(self):
+        text = '([1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0])'
+        values = parse_coordinate_basis(text)
+        expected = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
+        self.assertTrue(np.allclose(expected, values))
+
+    def test_crd_basis_mix_tuple_list_missing_crd(self):
+        text = '[ [1.0, 2.0, 3.0], (4.0), (7.0, 8, 9.0) ] '
+        with self.assertRaises(ValueError):
+            parse_coordinate_basis(text)
+
+    def test_crd_basis_multiple_nested_lists(self):
+        text = '[[[ [3.0, 2.0, 1.0] ]]] '
+        values = parse_coordinate_basis(text)
+        expected = [[3.0, 2.0, 1.0]]
+        self.assertTrue(np.allclose(values, expected))
 
 
 if __name__ == '__main__':
