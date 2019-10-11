159aaf356a510f960a4b6e47016eb983fad285e5
David Kleiven <davidkleiven446@gmail.com>
2019-10-11 16:45:27 +0200

    ENH: montecarlo: implement metadynamics

--- clease/montecarlo/__init__.py
+++ clease/montecarlo/__init__.py
@@ -1,5 +1,8 @@
 from clease.montecarlo.bias_potential import BiasPotential
 from clease.montecarlo.montecarlo import Montecarlo
 from clease.montecarlo.sgc_montecarlo import SGCMonteCarlo
+from clease.montecarlo.gaussian_kernel_bias_potential import GaussianKernelBiasPotential
+from clease.montecarlo.binned_bias_potential import BinnedBiasPotential
+from clease.montecarlo.metadynamics_sampler import MetaDynamicsSampler
 
 __all__ = ['Montecarlo']
--- clease/montecarlo/constraints/__init__.py
+++ clease/montecarlo/constraints/__init__.py
@@ -2,3 +2,4 @@ from clease.montecarlo.constraints.mc_constraint import MCConstraint
 from clease.montecarlo.constraints.constrain_swap_by_basis import ConstrainSwapByBasis
 from clease.montecarlo.constraints.constrain_element_insert_by_basis import ConstrainElementInserts
 from clease.montecarlo.constraints.fixed_element import FixedElement
+from clease.montecarlo.constraints.collective_variable_constraint import CollectiveVariableConstraint
