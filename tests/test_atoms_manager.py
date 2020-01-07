d2ae126dcb2671dc5745dee553b9ca8de0d6a459
David Kleiven <davidkleiven446@gmail.com>
2020-01-07 17:36:18 +0000

    refactor: Remove unused functions in AtomsManager

--- tests/test_atoms_manager.py
+++ tests/test_atoms_manager.py
@@ -114,42 +114,6 @@ class TestAtomsManager(unittest.TestCase):
         with self.assertRaises(ValueError):
             manager.tag_indices_of_corresponding_atom(prim_cell)
 
-    def test_cubic_sc_fcc(self):
-        a = 4.05
-        atoms = bulk("Al", a=a, crystalstructure="fcc")
-        manager = AtomsManager(atoms)
-        sc = manager.close_to_cubic_supercell()
-        expected_cell = np.array([[a, 0, 0], [0, a, 0], [0, 0, a]])
-        self.assertTrue(np.allclose(expected_cell, sc.get_cell()))
-
-    def test_sp217(self):
-        a = 10.553
-        b = 10.553
-        c = 10.553
-        alpha = 90
-        beta = 90
-        gamma = 90
-        cellpar = [a, b, c, alpha, beta, gamma]
-        basis = [(0, 0, 0), (0.324, 0.324, 0.324),
-                 (0.3582, 0.3582, 0.0393), (0.0954, 0.0954, 0.2725)]
-
-        atoms = crystal(symbols=['Mg', 'Mg', 'Mg', 'Al'],
-                        cellpar=cellpar, spacegroup=217,
-                        basis=basis, primitive_cell=True)
-
-        manager = AtomsManager(atoms)
-        sc = manager.close_to_cubic_supercell()
-        expected_cell = np.array([[a, 0, 0], [0, a, 0], [0, 0, a]])
-        self.assertTrue(np.allclose(expected_cell, sc.get_cell()))
-
-    def test_bcc(self):
-        a = 3.8
-        atoms = bulk("Fe", a=a, crystalstructure="bcc")
-        manager = AtomsManager(atoms)
-        sc = manager.close_to_cubic_supercell()
-        expected_cell = np.array([[a, 0, 0], [0, a, 0], [0, 0, a]])
-        self.assertTrue(np.allclose(sc.get_cell(), expected_cell))
-
 
 if __name__ == '__main__':
     unittest.main()
