d0a35c7419ce37a2ca81ecd1d45bc3e3ccc6835f
David Kleiven <davidkleiven446@gmail.com>
2020-02-26 07:27:59 +0000

    unittest: Add test case that confirms that the datamanager extracts the data in the same order as would be expected by using the ASE DB calls

--- clease/data_manager.py
+++ clease/data_manager.py
@@ -2,6 +2,12 @@ from clease import _logger
 from ase.db import connect
 import numpy as np
 from clease.tools import add_file_extension
+from ase.db.core import parse_selection
+from typing import Tuple, List
+
+
+class InconsistentDataError(Exception):
+    pass
 
 
 class DataManager(object):
@@ -11,17 +17,18 @@ class DataManager(object):
 
     Parameters:
 
-    db_name: str
+    db_name:
         Name of the database
     """
-    def __init__(self, db_name):
+    def __init__(self, db_name: str):
         self.db_name = db_name
         self._X = None
         self._y = None
         self._feat_names = None
         self._target_name = None
 
-    def get_data(self, select_cond, feature_getter, target_getter):
+    def get_data(self, select_cond: List[tuple], feature_getter: callable,
+                 target_getter: callable) -> Tuple[np.ndarray, np.ndarray]:
         """
         Return the design matrix X and the target data y
 
@@ -31,42 +38,72 @@ class DataManager(object):
             List of tuples with selection conditions
             (e.g. [('converged', '='' True)])
 
-        feature_getter: Callable object
-            Callable object that returns a row in the design matrix
-            corresponding to an AtomsRow object from the ASE database. It
+        feature_getter:
+            Callable object that returns design matrix. It
             also needs a method get_feature_names that returns a name of
-            each feature.
+            each feature. The passed instance should take a list of IDs
+            as input argument and return a numpy array corresponding to
+            the design matrix
 
-        target_getter: Callable object
+        target_getter:
             Callable object that extracts the target value. The __call__
-            method takes the AtomsRow used to construct the row in the design
-            matrix
+            method takes a list of ids in the database and return a numpy
+            array with the target values (e.g. energy per atom)
 
         Example:
 
         >>> from clease.data_manager import (
         ... CorrelationFunctionGetter, FinalStructEnergyGetter,
         ... DataManager)
-        >>> feat_getter = CorrelationFunctionGetter(['c0', 'c1_0',
+        >>> from ase.db import connect
+        >>> from ase import Atoms
+        >>> from ase.calculators.emt import EMT
+        >>> db_name = 'somedb.db'
+        >>> db = connect(db_name)  # Make sure that the DB exists
+        >>> db.write(Atoms(), external_tables={'polynomial_cf': {'c0': 1.0}}, final_struct_id=2, converged=True)
+        1
+        >>> final = Atoms('Cu')
+        >>> final.set_calculator(EMT())
+        >>> _ = final.get_potential_energy()
+        >>> db.write(final)
+        2
+        >>> feat_getter = CorrelationFunctionGetter(db_name, ['c0', 'c1_0',
         ... 'c2_d0000_0_00'], 'polynomial_cf')
-        >>> targ_getter = FinalStructEnergyGetter()
-        >>> manager = DataManager('somedb.db')
-        >>> X, y = manager.get_data([], feat_getter, targ_getter)
+        >>> targ_getter = FinalStructEnergyGetter(db_name)
+        >>> manager = DataManager(db_name)
+        >>> X, y = manager.get_data([('converged', '=', 1)], feat_getter, targ_getter)
         """
-        cfm = []
-        target = []
+        keys, cmps = parse_selection(select_cond)
         db = connect(self.db_name)
-        for row in db.select(select_cond):
-            cfm.append(feature_getter(row, db))
-            target.append(target_getter(row, db))
+        sql, args = db.create_select_statement(keys, cmps)
+        sql = sql.replace('systems.*', 'systems.id')
+        with connect(self.db_name) as db:
+            con = db.connection
+            cur = con.cursor()
+            cur.execute(sql, args)
+            ids = [row[0] for row in cur.fetchall()]
+        ids.sort()
+        cfm = feature_getter(ids)
+        target = target_getter(ids)
 
         self._X = np.array(cfm, dtype=float)
         self._y = np.array(target)
         self._feat_names = feature_getter.names()
         self._target_name = target_getter.name()
+
+        nrows = self._X.shape[0]
+        num_data = len(self._y)
+        if nrows != num_data:
+            raise InconsistentDataError(
+                f"Num. rows in deisgn matrix {nrows}."
+                f"Num. data points {num_data}\n"
+                "This is normally caused by a non-converged structure "
+                "being extracted from database. Check that all structures"
+                "extracted from DB using your query has a corresponding"
+                "final structure.")
         return self._X, self._y
 
-    def to_csv(self, fname):
+    def to_csv(self, fname: str):
         """
         Export the dataset used to fit a model y = Xc where y is typically the
         DFT energy per atom and c is the unknown ECIs. This function exports
@@ -128,8 +165,16 @@ class CorrFuncEnergyDataManager(DataManager):
     CorrFuncFinalEnergyDataManager is a convenience class provided
     to handle the standard case where the features are correlation functions
     and the target is the DFT energy per atom
+
+    Parameters
+
+    db_name: Name of the database being passed
+
+    cf_names: List with the correlation function names to extract
+
+    tab_name: Name of the table where the correlation functions are stored
     """
-    def __init__(self, db_name, cf_names, tab_name):
+    def __init__(self, db_name: str, cf_names: List[str], tab_name: str):
         DataManager.__init__(self, db_name)
         self.tab_name = tab_name
         self.cf_names = cf_names
@@ -147,8 +192,8 @@ class CorrFuncEnergyDataManager(DataManager):
         """
         return DataManager.get_data(
             self, select_cond,
-            CorrelationFunctionGetter(self.cf_names, self.tab_name),
-            FinalStructEnergyGetter())
+            CorrelationFunctionGetter(self.db_name, self.cf_names, self.tab_name),
+            FinalStructEnergyGetter(self.db_name))
 
 
 class CorrelationFunctionGetter(object):
@@ -158,6 +203,8 @@ class CorrelationFunctionGetter(object):
 
     Parameters:
 
+    db_name:
+        Name of the database
     cf_names: list
         List with the names of the correlation functions
 
@@ -165,7 +212,8 @@ class CorrelationFunctionGetter(object):
         Name of the external table where the correlation functions
         are stored
     """
-    def __init__(self, cf_names, tab_name):
+    def __init__(self, db_name: str, cf_names: List[str], tab_name: str):
+        self.db_name = db_name
         self.cf_names = cf_names
         self.tab_name = tab_name
 
@@ -175,8 +223,46 @@ class CorrelationFunctionGetter(object):
         """
         return self.cf_names
 
-    def __call__(self, row, db):
-        return [row[self.tab_name][x] for x in self.cf_names]
+    def __call__(self, ids: List[int]) -> np.ndarray:
+        id_seq = ','.join(['?']*len(ids))
+        name_seq = ','.join(['?']*len(self.cf_names))
+        sql = f"SELECT * FROM {self.tab_name} WHERE id IN ({id_seq}) AND key IN ({name_seq})"
+        args = ids + self.cf_names
+        id_cf_values = {}
+        id_cf_names = {}
+        with connect(self.db_name) as db:
+            con = db.connection
+            cur = con.cursor()
+            cur.execute(sql, args)
+
+            for row in cur.fetchall():
+                name = row[0]
+                value = row[1]
+                dbId = row[2]
+
+                cur_row = id_cf_values.get(dbId, [])
+                names = id_cf_names.get(dbId, [])
+
+                cur_row.append(value)
+                names.append(name)
+
+                id_cf_values[dbId] = cur_row
+                id_cf_names[dbId] = names
+
+        # Make sure that all the rows are ordered in the same way
+        key = list(id_cf_names.keys())[0]
+        self._cf_names = id_cf_names[key]
+        cf_name_col = {n: i for i, n in enumerate(id_cf_names[key])}
+
+        cf_matrix = np.zeros((len(id_cf_values), len(cf_name_col)))
+        row = 0
+        for row, dbId in enumerate(ids):
+            cf_values = id_cf_values[dbId]
+            cf_names = id_cf_names[dbId]
+            cols = [cf_name_col[name] for name in cf_names]
+            cf_matrix[row, :] = np.array(cf_values)[cols]
+            row += 1
+        return cf_matrix
 
 
 class FinalStructEnergyGetter(object):
@@ -185,14 +271,38 @@ class FinalStructEnergyGetter(object):
     (typically after structure relaxation) corresponding to the passed
     AtomsRow object.
     """
+    def __init__(self, db_name: str):
+        self.db_name = db_name
+
     def name(self):
         return "E_DFT (eV/atom)"
 
-    def __call__(self, row, db):
-        final_struct_id = row.get("final_struct_id", None)
+    def __call__(self, ids: List[int]) -> np.ndarray:
+        id_seq = ','.join(['?']*len(ids))
+        sql = f"SELECT value, id FROM number_key_values WHERE key='final_struct_id' AND id IN ({id_seq})"
 
-        if final_struct_id is None:
-            msg = f"The passed atoms row {row} has no final_struct_id"
-            raise ValueError(msg)
+        # Map beetween the initial structure and the index in the id list
+        init_struct_idx = {idx: i for i, idx in enumerate(ids)}
 
-        return db.get(final_struct_id).energy/row.natoms
+        with connect(self.db_name) as db:
+            con = db.connection
+            cur = con.cursor()
+            cur.execute(sql, ids)
+            final_struct_ids = []
+
+            # Map the between final struct id and initial struct id
+            init_struct_ids = {}
+            for row in cur.fetchall():
+                final_id, init_id = row
+                final_struct_ids.append(final_id)
+                init_struct_ids[final_id] = init_id
+            final_struct_id_seq = ','.join(['?']*len(final_struct_ids))
+            sql = f"SELECT id, energy, natoms FROM systems WHERE id IN ({final_struct_id_seq})"
+            cur.execute(sql, final_struct_ids)
+            energies = np.zeros(len(final_struct_ids))
+            for row in cur.fetchall():
+                final_id, energy, natoms = row
+                init_id = init_struct_ids[final_id]
+                idx = init_struct_idx[init_id]
+                energies[idx] = energy/natoms
+        return energies
--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -2,8 +2,13 @@ import unittest
 from ase.db import connect
 from ase.calculators.emt import EMT
 from ase.build import bulk
+from ase import Atoms
 from clease import CorrFuncEnergyDataManager
+from clease.data_manager import InconsistentDataError
+from clease.tools import update_db
+from ase.calculators.singlepoint import SinglePointCalculator
 import numpy as np
+from random import shuffle
 import os
 
 
@@ -29,7 +34,6 @@ class TestDataManager(unittest.TestCase):
         cf_names = ['c0', 'c1_1', 'c2_d0000_0_00']
         manager = CorrFuncEnergyDataManager(db_name, cf_names, 'cf_func')
         X, y = manager.get_data([('converged', '=', 1)])
-        os.remove(db_name)
 
         expect_X = np.zeros((10, 3))
         expect_X[:, 0] = 0.0
@@ -49,6 +53,16 @@ class TestDataManager(unittest.TestCase):
         os.remove(csvfile)
         self.assertTrue(np.allclose(X, X_read[:, :-1]))
         self.assertTrue(np.allclose(y, X_read[:, -1]))
+
+        # Add an initial structure that is by mistake labeled as converged
+        db = connect(db_name)
+        db.write(Atoms(), converged=True,
+                 external_tables={'cf_func': {k: 1.0 for k in cf_names}})
+
+        with self.assertRaises(InconsistentDataError):
+            X, y = manager.get_data([('converged', '=', 1)])
+
+        os.remove(db_name)
 
     def test_get_pattern(self):
         db_name = 'test_get_pattern.db'
@@ -111,6 +125,47 @@ class TestDataManager(unittest.TestCase):
         for t in tests:
             res = manager.get_cols(t['names'])
             self.assertTrue(np.allclose(res, t['expect']))
+
+    def test_consistent_order(self):
+        db_name = 'test_consistent_order.db'
+        db = connect(db_name)
+
+        init_struct_ids = []
+        for i in range(10):
+            atoms = bulk('Cu')*(3, 3, 3)
+            cf_func = {'c0': np.random.rand(), 'c1_1': np.random.rand()}
+            dbId = db.write(atoms, converged=True, name=f"structure{i}",
+                            external_tables={'cf_func': cf_func})
+            init_struct_ids.append(dbId)
+
+        # Add final structures in a random order
+        shuffle(init_struct_ids)
+        for init_id in init_struct_ids:
+            atoms = db.get(id=init_id).toatoms()
+            calc = SinglePointCalculator(atoms, energy=np.random.rand())
+            atoms.set_calculator(calc)
+            update_db(uid_initial=init_id, final_struct=atoms, db_name=db_name)
+
+        cf_names = list(cf_func.keys())
+        manager = CorrFuncEnergyDataManager(db_name, cf_names, 'cf_func')
+        query = [('converged', '=', 1)]
+        X, y = manager.get_data(query)
+
+        # Extract via ASE calls
+        X_ase = []
+        y_ase = []
+        for row in db.select(query):
+            x_row = [row['cf_func'][n] for n in cf_names]
+            X_ase.append(x_row)
+
+            fid = row.final_struct_id
+            final_row = db.get(id=fid)
+            y_ase.append(final_row.energy/final_row.natoms)
+
+        self.assertTrue(np.allclose(X_ase, X))
+        self.assertTrue(np.allclose(y, y_ase))
+        os.remove(db_name)
+
 
 if __name__ == '__main__':
     unittest.main()
