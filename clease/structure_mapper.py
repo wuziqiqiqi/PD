b8268a59898959b74dad64b61efe27e8c6fd2a7a
David Kleiven <davidkleiven446@gmail.com>
2020-05-04 14:02:25 +0000

    BUG: Ensure consistent stoichiometry by using MIC distance instead of extended template. Remove unused function

--- clease/structure_mapper.py
+++ clease/structure_mapper.py
@@ -1,9 +1,9 @@
 from ase.atoms import Atoms, Cell
 import spglib
 import numpy as np
-from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 from typing import Tuple
+from ase.geometry import find_mic
 
 
 class TransformInfo(object):
@@ -56,33 +56,6 @@ class StructureMapper(object):
         return Atoms(numbers=numbers, cell=lattice,
                      scaled_positions=scaled_pos, pbc=[1, 1, 1])
 
-    def _extend_atoms(self, atoms: Atoms, buffer: float = 0.01) -> Atoms:
-        """
-        Adds periodic images within buffer
-
-        :param atoms: Atoms object that will be extended
-        :param buffer: Buffer in scaled coordinates
-        """
-        atoms_cpy = atoms.copy()
-        for atom in atoms_cpy:
-            atom.tag = atom.index
-        atoms_cpy = atoms_cpy*(3, 3, 3)
-        scaled_pos = atoms_cpy.get_scaled_positions()
-        filtered_pos = []
-        filtered_numbers = []
-        filtered_tags = []
-        for i in range(scaled_pos.shape[0]):
-            x = scaled_pos[i, :]
-            if np.all(x > 1.0/3.0 - buffer) and np.all(x < 2.0/3.0 + buffer):
-                filtered_pos.append(x)
-                filtered_numbers.append(atoms_cpy.numbers[i])
-                filtered_tags.append(atoms_cpy[i].tag)
-
-        filtered_pos = np.array(filtered_pos)*3.0 - 1.0
-        cartesian_pos = atoms.get_cell().dot(filtered_pos.T).T
-        return Atoms(numbers=filtered_numbers, positions=cartesian_pos,
-                     tags=filtered_tags)
-
     def strain(self, cell1: Cell, cell2: Cell) -> np.ndarray:
         """
         Calculate the strain tensor to map cell1 into cell2.
@@ -105,36 +78,33 @@ class StructureMapper(object):
         relaxed atoms object is less than the number of atoms in the template,
         the method will add vacancies on sites that are not matched.
 
-        :param atoms: Relaxed atoms object
-        :param template: Ideal lattice
+        :param atoms: Relaxed atoms object. Note that the cell and atomic
+            positions will be altered.
+        :param template: Ideal lattice. On return, the symbols corresponds
+            to the mapped symbols in atoms
         """
         strain = self.strain(atoms.get_cell(), template.get_cell())
         atoms.set_cell(template.get_cell(), scale_atoms=True)
-        extended_template = self._extend_atoms(template)
-        pos1 = atoms.get_positions()
-        pos2 = extended_template.get_positions()
+
+        # Build distance matrix using MIC
+        distances = np.zeros((len(atoms), len(template)))
+        cell = template.get_cell()
+        all_dist_vec = []
+        for i in range(len(atoms)):
+            for j in range(len(template)):
+                d = template[j].position - atoms[i].position
+                all_dist_vec.append(d)
+        _, micDist = find_mic(all_dist_vec, cell)
+        distances = micDist.reshape((len(atoms), len(template)))
 
         # Snap atoms to ideal sites such that collective distance
         # is minimized
-        distances = cdist(pos1, pos2)
         row, assignment = linear_sum_assignment(distances)
         displacements = distances[row, assignment]
-        template_copy = extended_template.copy()
-        template_copy.numbers = np.zeros(len(template_copy))
-        for i in range(len(atoms)):
-            template_copy[assignment[i]].symbol = atoms[i].symbol
-
-        snapped = template.copy()
-        populated = np.zeros(len(snapped), dtype=np.uint8)
-        for i, atom in enumerate(template_copy):
-            if not populated[atom.tag]:
-                snapped[atom.tag].symbol = atom.symbol
-                populated[i] = 1
-            else:
-                if snapped[atom.tag].symbol == 'X':
-                    snapped[atom.tag].symbol = atom.symbol
+        template.numbers = np.zeros(len(template))
+        template.numbers[assignment] = atoms.numbers
 
         transform_info = TransformInfo()
         transform_info.displacements = displacements
         transform_info.strain = strain
-        return snapped, transform_info
+        return template, transform_info
