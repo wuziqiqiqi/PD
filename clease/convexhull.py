BUG: fix singlet normalisation for systems with background species
Fix a bug for background species handling. This commit makes the singlet
correlation functions to always normalise to the number of active species.
Hence, any background was treated as if it does not exists. Additional
properties have been added to the `ClusterExpansionSettings` for recoverying
the original atomic based concentrations. Coefficients have been updated
in the consistency tests.

Please note that the changes made here means existing expansions with background
lattices needs to have their correlation functions regenerated and ECI refitted.
Otherwise it will give rise to problem in MC simulations.

clease/convexhull.py
@@ -263,7 +263,7 @@ class ConvexHull:
             x = np.array(concs[elems[i]])
             x /= self.conc_scale
 
-            ax.plot(x, energies * self.atoms_per_fu, marker, mfc=mfc)
+            ax.plot(x, np.asarray(energies) * self.atoms_per_fu, marker, mfc=mfc)
 
             if self.atoms_per_fu > 1:
                 unit = "eV/f.u."