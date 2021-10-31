d7ee88fe21e04c3a61c10cc22bfb5a7b824b4c4c
Alexander Tygesen <atygesen@outlook.dk>
2021-10-31 16:40:39 +0100

    STYLE: Import * in init.py rather than explicit class

--- clease/datastructures/__init__.py
+++ clease/datastructures/__init__.py
@@ -2,7 +2,7 @@
 
 from .system_changes import *
 from .four_vector import *
-from .figure import Figure
+from .figure import *
 
 ADDITIONAL = ('system_changes',)
 __all__ = system_changes.__all__ + four_vector.__all__ + figure.__all__ + ADDITIONAL
