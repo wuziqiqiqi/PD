19a379246130fac835fa973452a88493bc922936
Alexander Tygesen <atygesen@outlook.dk>
2022-05-31 10:52:29 +0000

    MAINT: Sphinx 5.0.0 now requires language to be set

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -83,7 +83,8 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# As of sphinx 5.0.0, this must be set, as otherwise a warning is generated.
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
