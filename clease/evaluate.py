BUG: Residual plot has wrong units

clease/evaluate.py
@@ -5,7 +5,7 @@ import sys
 import json
 import logging as lg
 import multiprocessing as mp
-from typing import Optional, Dict, List
+from typing import Dict, List
 
 import numpy as np
 from ase.db import connect
@@ -1165,30 +1165,6 @@ class Evaluate:
         if self.eci is None:
             raise ValueError("ECIs have not been fitted yet.")
         return self.cf_matrix.dot(self.eci)
-
-    def subtract_predict_dft(self) -> np.ndarray:
-        """
-        Subtract CE predicted energy from DFT energy
-
-        :return: Energy difference between DFT and CE model
-            (i.e., E_DFT - E_CE) in the original unit (typically eV)
-        """
-        e_pred = self.get_energy_predict()
-        delta_e = self.e_dft - e_pred
-        return delta_e
-
-    def subtract_predict_dft_loo(self) -> Optional[np.ndarray]:
-        """
-        Subtract CE predicted energy from DFT energy in the leave-one-out
-        scheme.
-
-        :return: Energy difference between DFT and CE model
-            (i.e., E_DFT - E_CE_LOO) in the original unit (typically eV)
-        """
-        if self.e_pred_loo is None:
-            return None
-        delta_e_loo = self.e_dft - self.e_pred_loo
-        return delta_e_loo
 
     def get_eci_by_size(self) -> Dict[str, Dict[str, list]]:
         """

clease/plot_post_process.py
@@ -105,7 +105,8 @@ def plot_fit_residual(evaluate: Evaluate, plot_args: dict = None) -> Figure:
     if plot_args is None:
         plot_args = {}
     X = evaluate.e_dft
-    Y = evaluate.subtract_predict_dft()
+    Y = evaluate.get_energy_predict() - X  # eV/atom
+    Y *= 1000  # meV/atom
     xlabel = plot_args.get("xlabel", "#OCC")
     ylabel = plot_args.get("ylabel", r"$E_{DFT} - E_{pred}$ (meV/atom)")
     title = plot_args.get("title", "Residual (v)")
@@ -116,6 +117,7 @@ def plot_fit_residual(evaluate: Evaluate, plot_args: dict = None) -> Figure:
     ax[0].set_ylabel(ylabel)
     ax[0].axhline(0, ls="--")
     ax[0].plot(X, Y, "v", mfc="none")
+    ax[0].set_xlabel(r"$E_{DFT}$ (eV/atom)")
 
     hist, bin_edges = np.histogram(Y, bins=30)
     h = bin_edges[1] - bin_edges[0]

tests/test_plotpostprocess.py
@@ -36,14 +36,14 @@ def test_plot_fit_residual(bc_setting):
     evaluator = Evaluate(bc_setting, fitting_scheme="l2", alpha=1e-6)
     evaluator.get_eci()
 
-    # y-axis values calculated by subtract_predict_dft
-    predict["delta_e"] = np.ndarray.tolist(evaluator.subtract_predict_dft())
+    delta_e = evaluator.get_energy_predict() - evaluator.e_dft
+    predict["delta_e"] = delta_e * 1000  # convert to meV/atom
 
     fig = pp.plot_fit_residual(evaluator, plot_args)
     assert predict["title"] == fig.get_axes()[0].get_title()
     assert predict["xlabel"] == fig.get_axes()[1].get_xlabel()
     assert predict["ylabel"] == fig.get_axes()[0].get_ylabel()
-    assert predict["delta_e"] == np.ndarray.tolist(fig.get_children()[1].get_lines()[1].get_ydata())
+    assert np.allclose(predict["delta_e"], fig.get_children()[1].get_lines()[1].get_ydata())
 
 
 def test_plot_eci(bc_setting):